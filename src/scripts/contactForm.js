43dc3e00381cb0931557ac9fd64ca85eeb7facd7
Hunter Metts <huntermettsdev@gmail.com>
2018-12-18 09:43:52 -0600

    empty form

---
+++ src/scripts/contactForm.js
@@ -0,0 +1,24 @@
+const contactForm = {
+  buildEmptyContactForm(){
+    let userContactForm =
+    `<fieldset>
+        <label for="contact_name">Name:</label>
+        <input id="contact_name" name="contact_name" type="text" autofocus/>
+    </fieldset>
+    <fieldset>
+        <label for="contact_number">Number:</label>
+        <input id="contact_number" name="contact_number" type="text" autofocus/>
+    </fieldset>
+    <fieldset>
+        <label for="contact_address">Address:</label>
+        <input id="contact_address" name="contact_address" type="text" autofocus/>
+    </fieldset>
+ <button id="contact_save">Create New Contact</button>`
+
+ let indexHtmlArticle = document.querySelector(".formContainer")
+ indexHtmlArticle.innerHTML = userContactForm
+ console.log("test")
+  }
+}
+
+export default contactForm
