7ce810209946de756e7db9151406f56a4aa8a929
Hunter Metts <huntermettsdev@gmail.com>
2018-12-18 13:35:02 -0600

    MVP Met

--- src/scripts/contactCollection.js
+++ src/scripts/contactCollection.js
@@ -2,6 +2,18 @@ const contactCollection = {
   getAllEntries() {
     return fetch("http://localhost:8088/contact")
     .then(response => response.json())
+  },
+
+  postAnEntry(entryToSave){
+    return fetch("http://localhost:8088/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        // Where the argument is called and the object is passed that has been declared in eventListener.js
+        body: JSON.stringify(entryToSave)
+            })
+            .then(response => response.json())
   }
 }
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,9 @@
 import contactForm from "./contactForm"
 import contactList from "./contactList"
+import eventListener from "./eventListener"
+
 
 contactForm.buildEmptyContactForm();
+let saveButton = document.getElementById("contact_save")
+saveButton.addEventListener("click", eventListener.contactSaver)
 contactList.contactDisplay();
